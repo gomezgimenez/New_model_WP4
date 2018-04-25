64ffc5d60433bee43b5b8a3c24d971b03edeb6be
Marta Gomez Gimenez <mgomezgimenez@gmail.com>
2018-04-25 10:54:23 +0200

    comment again the raster/ reclass

--- wp_4_aidin01.R
+++ wp_4_aidin01.R
@@ -17,20 +17,20 @@ esa2015<-raster(paste0(getwd(),"/DATA/esa_lc_2015_agg10.tif"))
 esa<-stack(esa1992,esa2015)
 print('data_loaded')
 
-# Reclass to Anthropogenic Classes (i.e. Cultivated and Urban Areas)
-# See http://maps.elie.ucl.ac.be/CCI/viewer/download/CCI-LC_Maps_Legend.pdf
-m <- c(10,1,11,1,12,1,20,1,30,1,40,1,190,1,
-       50,NA,60,NA,61,NA,62,NA,70,NA,71,NA,72,NA,
-       80,NA,81,NA,82,NA,90,NA,100,NA,110,NA,
-       120,NA,121,NA,122,NA,130,NA,140,NA,
-       150,NA,151,NA,152,NA,153,NA,160,NA,
-       170,NA,180,NA,190,NA,200,NA,201,NA,
-       202,NA,210,NA,220,NA)
-print('m_1')
-rclmat <- matrix(m, ncol=2, byrow=TRUE)
-print('m_2')
-esa <- reclass(esa, rclmat,na.rm=T)
-print('data_classified')
+# # Reclass to Anthropogenic Classes (i.e. Cultivated and Urban Areas)
+# # See http://maps.elie.ucl.ac.be/CCI/viewer/download/CCI-LC_Maps_Legend.pdf
+# m <- c(10,1,11,1,12,1,20,1,30,1,40,1,190,1,
+#        50,NA,60,NA,61,NA,62,NA,70,NA,71,NA,72,NA,
+#        80,NA,81,NA,82,NA,90,NA,100,NA,110,NA,
+#        120,NA,121,NA,122,NA,130,NA,140,NA,
+#        150,NA,151,NA,152,NA,153,NA,160,NA,
+#        170,NA,180,NA,190,NA,200,NA,201,NA,
+#        202,NA,210,NA,220,NA)
+# print('m_1')
+# rclmat <- matrix(m, ncol=2, byrow=TRUE)
+# print('m_2')
+# esa <- reclass(esa, rclmat,na.rm=T)
+# print('data_classified')
 ## Calculate number of Antropogenic cells withing 100 x 100 grides
 f<-10
 #esa<-aggregate(esa,fact=f,fun=sum,na.rm=T)
